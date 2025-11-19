61371ecae61337a0a5fc8d9c752a88389a05b4e6
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-11-19 18:41:35 +0300

    w, fix)

--- actions/upload.php
+++ actions/upload.php
@@ -1,5 +1,7 @@
 <?php
 
-move_uploaded_file($_FILES['file']['tmp_name'], '../u/' . $_FILES['file']['name']);
+$cwd = dirname(getcwd());
+
+move_uploaded_file($_FILES['file']['tmp_name'], $cwd . '/u/' . $_FILES['file']['name']);
 
 header('Location: /');
